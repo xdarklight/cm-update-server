388e7b406975a4f6a895069ed3a3721ffb4fb0c5
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2013-12-29 13:15:30 +0100

    Add a migration which adds some basic indices.

---
+++ migrations/20131229040110-init.js
@@ -0,0 +1,25 @@
+module.exports = {
+	up: function(migration, DataTypes, done) {
+		migration.addIndex(
+			'Devices',
+			[ 'name' ],
+			{
+				indexName: 'Devices_UniqueName',
+				indicesType: 'UNIQUE',
+			}
+		);
+
+		migration.addIndex(
+			'Roms',
+			[ 'DeviceId', 'isActive', 'updateChannel' ],
+			{
+				indexName: 'Roms_FetchUpdateListIndex',
+			}
+		);
+	},
+	down: function(migration, DataTypes, done) {
+		migration.removeIndex('Devices', 'Devices_UniqueName');
+		migration.removeIndex('Roms', 'Roms_FetchUpdateListIndex');
+		done();
+	}
+}
