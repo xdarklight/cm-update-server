e5a9910c2e0e5e5c3a288b10d5f75d960643a0f4
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2013-12-31 19:10:32 +0100

    Description++

--- get-sourcecode-timestamp.js
+++ get-sourcecode-timestamp.js
@@ -2,7 +2,7 @@ var Troll = require('troll-opt').Troll;
 var models = require('./models/');
 
 var buildInfo = (new Troll()).options(function(troll) {
-	troll.banner('Outputs the newest source-code timestamp for the given device / subdirectory.');
+	troll.banner('Outputs the newest source-code as ISO-8601 string for the given device / subdirectory.');
 	troll.opt('device', 'The device ID.', { type: 'string', required: true });
 	troll.opt('subdirectory', 'The subdirectory from which the file can be downloaded.', { type: 'string' });
 });
