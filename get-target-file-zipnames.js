ca10e24d578c1a091b815deb522fc0e656673020
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2014-01-10 22:58:10 +0100

    Add a "max_age_days" filter parameter to "get-target-file-zipnames".

--- get-target-file-zipnames.js
+++ get-target-file-zipnames.js
@@ -5,15 +5,30 @@ var buildInfo = (new Troll()).options(function(troll) {
 	troll.banner('Outputs all target-files zip names (one per line) for all builds that match the given device / subdirectory.');
 	troll.opt('device', 'The device ID.', { type: 'string', required: true });
 	troll.opt('subdirectory', 'The subdirectory from which the file can be downloaded.', { type: 'string' });
+	troll.opt('max_age_days', 'The max age (according to a Rom\' timestamp) of the target files in days.', { type: 'integer' });
 });
 
+var minDate = new Date(-8640000000000000);
+
 models.sequelize.sync().success(function() {
+	var startTimestamp;
+
+	if (!buildInfo.max_age_days || isNaN(buildInfo.max_age_days)) {
+		startTimestamp = minDate;
+	} else {
+		startTimestamp = new Date();
+		startTimestamp.setHours(-24 * buildInfo.max_age_days);
+	}
+
 	models.Rom.findAll({
 		include: [
 			{ model: models.Device, where: { name: buildInfo.device } },
 		],
 		where: {
 			subdirectory: buildInfo.subdirectory,
+			timestamp: {
+				gt: startTimestamp,
+			}
 		},
 	}).success(function(roms) {
 		roms.forEach(function(rom) {
