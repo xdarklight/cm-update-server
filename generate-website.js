abb489aea947f8815fe8a9df8b08c69154474fd4
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2014-08-29 20:07:13 +0200

    Website: Refactored website building tasks to async.js.

--- generate-website.js
+++ generate-website.js
@@ -1,21 +1,10 @@
 var fsextra = require('fs-extra');
 var path = require('path');
+var async = require('async');
 var wintersmith = require('wintersmith');
 var config = require('config').Website;
 var models = require('./models/');
 var ResultConverter = require('./result-converter.js');
-
-var generateWebsite = function() {
-	var env = wintersmith(config);
-
-	env.build(function(error) {
-		if (error) {
-			throw error;
-		}
-
-		console.log('Finished building website!');
-	});
-}
 
 models.sequelize.sync().success(function() {
 
@@ -28,36 +17,56 @@ models.sequelize.sync().success(function() {
 	fsextra.ensureDirSync(deviceJsonPath);
 	fsextra.ensureDirSync(config.output);
 
-	models.Device.findAll({ order: 'name ASC' }).success(function(devices) {
-		devices.forEach(function(device, deviceIndex) {
-			var deviceValues = device.toJSON();
-			deviceValues.template = 'device.jade';
-			deviceValues.filename = '/device-' + device.name + '.html';
-
-			models.Rom.findAll({
-				where: {
-					DeviceId: device.id,
-					isActive: true,
-				},
-				order: 'createdAt DESC',
-			}).success(function(roms) {
-				deviceValues.roms = [];
-
-				roms.forEach(function(rom) {
-					var romValues = rom.toJSON();
-					romValues.downloadUrl = ResultConverter.getRomDownloadUrl(rom);
-					romValues.md5sumUrl = ResultConverter.getRomMd5sumUrl(rom);
-
-					deviceValues.roms.push(romValues);
-				});
-
-				fsextra.writeFileSync(path.join(deviceJsonPath, device.id + '.json'), JSON.stringify(deviceValues));
-			}).success(function() {
-				// Generate the website after the roms of the last device were processed.
-				if (deviceIndex == devices.length -1) {
-					generateWebsite();
-				}
+	async.waterfall([
+		function(waterfallCallback) {
+			models.Device.findAll({ order: 'name ASC' }).complete(function(error, devices) {
+				waterfallCallback(error, devices);
 			});
-		});
-	});
+		},
+
+		function(devices, waterfallCallback) {
+			async.each(devices, function(device, eachCallback) {
+				var deviceValues = device.toJSON();
+				deviceValues.template = 'device.jade';
+				deviceValues.filename = '/device-' + device.name + '.html';
+
+				models.Rom.findAll({
+					where: {
+						DeviceId: device.id,
+						isActive: true,
+					},
+					order: 'createdAt DESC',
+				}).complete(function(err, roms) {
+					deviceValues.roms = [];
+
+					roms.forEach(function(rom) {
+						var romValues = rom.toJSON();
+						romValues.downloadUrl = ResultConverter.getRomDownloadUrl(rom);
+						romValues.md5sumUrl = ResultConverter.getRomMd5sumUrl(rom);
+
+						deviceValues.roms.push(romValues);
+					});
+
+					fsextra.writeFileSync(path.join(deviceJsonPath, device.id + '.json'), JSON.stringify(deviceValues));
+
+					eachCallback(err, null);
+				});
+			}, waterfallCallback);
+		},
+
+
+		function(waterfallCallback) {
+			var env = wintersmith(config);
+
+			env.build(function(error) {
+				if (error) {
+					console.log('ERROR while building website: ' + error);
+				} else {
+					console.log('Finished building website!');
+				}
+
+				waterfallCallback(error, null);
+			});
+		},
+	]);
 });
